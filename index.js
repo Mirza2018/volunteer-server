c7e8c8e0c0a752dc8b541826328715e7e924646e
unknown <mirza.eee.4th@gmail.com>
2023-09-23 07:52:24 +0600

    get volenteer

--- index.js
+++ index.js
@@ -50,6 +50,10 @@ async function run() {
             const result = await donationCollection.insertOne(infos);
             res.send(result)
         })
+        app.get('/volunteerpage', async (req, res) => {
+            const result = await donationCollection.find().toArray()
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
