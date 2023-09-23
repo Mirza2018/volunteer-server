f358aa69749b9016ce2447d164bb46b7fb182032
unknown <mirza.eee.4th@gmail.com>
2023-09-23 07:47:34 +0600

    post donation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -29,9 +29,29 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const volunteerCollection = client.db("volunteerDB").collection("volunteer");
+        const donationCollection = client.db("volunteerDB").collection("donation");
+
+        app.get('/volunteer', async (req, res) => {
+            const result = await volunteerCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/volunteer/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) }
+            const result = await volunteerCollection.findOne(filter);
+            res.send(result)
+        })
+
+        app.post('/donation', async (req, res) => {
+            const infos = req.body;
+            const result = await donationCollection.insertOne(infos);
+            res.send(result)
+        })
 
 
-        
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
